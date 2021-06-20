4f2b7351837fa62eb144b97c75a4a82916a61982
e.popov <e.popov@fun-box.ru>
2021-06-20 20:21:18 +0500

    Added json-ld parsing

--- phone_parser.py
+++ phone_parser.py
@@ -1,7 +1,9 @@
 import argparse
 import asyncio
+import json
 import re
-from typing import Iterable, Optional, List
+from json import JSONDecodeError
+from typing import List, Optional
 
 import httpx as httpx
 from bs4 import BeautifulSoup
@@ -12,7 +14,7 @@ phone_pattern = re.compile(r"(?:\+7|8)([\s\d()-]*)")
 trash_pattern = re.compile(r"[\s()-]*")
 
 
-def find_phones(raw_text: str) -> Iterable[str]:
+def find_phones(raw_text: str) -> list[str]:
     def _clean_phone(dirty_phone: str) -> str:
         cleaned = trash_pattern.sub("", dirty_phone)
         if len(cleaned) == 7:
@@ -20,7 +22,7 @@ def find_phones(raw_text: str) -> Iterable[str]:
         elif len(cleaned) == 10:
             return "8{}".format(cleaned)
 
-    return filter(bool, map(_clean_phone, phone_pattern.findall(raw_text)))
+    return list(filter(bool, map(_clean_phone, phone_pattern.findall(raw_text))))
 
 
 async def get_html(url: str) -> Optional[str]:
@@ -30,9 +32,26 @@ async def get_html(url: str) -> Optional[str]:
             return r.text
 
 
-def parse_html(html: str) -> Iterable[str]:
+def find_phones_in_schema(soup) -> List[str]:
+    schema = soup.findAll("script", attrs={"type": "application/ld+json"})
+    phones = []
+    for item in schema:
+        try:
+            meta_data = json.loads(item.string)
+        except JSONDecodeError:
+            continue
+
+        if "telephone" in meta_data:
+            ph = find_phones(meta_data["telephone"])
+            phones.extend(ph)
+    return phones
+
+
+def parse_html(html: str) -> List[str]:
     soup = BeautifulSoup(html, "lxml")
     phones = []
+    phones.extend(find_phones_in_schema(soup))
+
     for tag_content in soup.body.stripped_strings:
         phones.extend(find_phones(tag_content))
     return phones
